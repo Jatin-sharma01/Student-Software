03e8e407885886b01d50132b4abf9b2a8183f270
Jatin-sharma01 <amobsidian001@gmail.com>
2025-08-11 13:55:49 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainScreen from './containers/MainScreen';
-import Signup from './containers/Student/Signup';
-import Teacher from './containers/Teacher/Signup';
-import Login from './containers/Student/Loggedin';
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import MainScreen from "./containers/MainScreen";
+import Signup from "./containers/Student/Signup";
+import Teacher from "./containers/Teacher/Signup";
+import Login from "./containers/Student/Loggedin";
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<MainScreen/>}/>  
-      <Route path="/student-signup" element={<Signup/>}/>  
-      <Route path="/teacher-signup" element={<Teacher/>}/>  
-      <Route path="/loginmenu" element={<Login/>}/>  
+        <Route path="/" element={<MainScreen />} />
+        <Route path="/student-signup" element={<Signup />} />
+        <Route path="/teacher-signup" element={<Teacher />} />
+        <Route path="/loginmenu" element={<Login />} />
       </Routes>
     </Router>
   );
